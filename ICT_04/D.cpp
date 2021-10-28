35b73b8df6edb39ea19a01c8d682a842cbe819ab
Mohammad Huzaifa <78971588+Huzaifa17@users.noreply.github.com>
2021-10-28 22:00:12 +0600

    Create D.cpp

---
+++ ICT_04/D.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int i, j, k, n, m, t;
+	cin >> n;
+	m = n % 100;
+	cout << 100 - m << endl;
+	return 0;
+}
