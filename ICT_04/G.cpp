8051c203d920e5c3a1fe73282b209e91173c01ab
Mohammad Huzaifa <78971588+Huzaifa17@users.noreply.github.com>
2021-10-28 22:43:29 +0600

    Create G.cpp

---
+++ ICT_04/G.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int i, j, k, n, m, t;
+	double a, b;
+	cin >> a >> b;
+	b = (a - b) * 100;
+	a = b / a;
+	cout << a << endl;
+	return 0;
+}
